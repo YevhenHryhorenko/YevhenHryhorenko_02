d8c93fa2fc0aed78f51dae42538ef7a2918e9ffc
YevhenHryhorenko <mrhryhorenko@gmail.com>
2023-02-28 14:36:42 +0200

    create script "+, -, *, /"

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNum = +prompt("Enter first number: "),
+	secondNum = +prompt("Enter second number: ");
+
+alert(`
+${firstNum} + ${secondNum} = ${firstNum + secondNum}
+${firstNum} - ${secondNum} = ${firstNum - secondNum}
+${firstNum} * ${secondNum} = ${firstNum * secondNum}
+${firstNum} / ${secondNum} = ${firstNum / secondNum}
+`);
