68a1475cfe6b31ab141dd3ba0c2eebd9d71ee63f
YevhenHryhorenko <mrhryhorenko@gmail.com>
2023-02-28 17:41:28 +0200

    add division check

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 let firstNum = +prompt("Enter first number: "),
-	secondNum = +prompt("Enter second number: ");
+	secondNum = +prompt("Enter second number: "),
+	division = `${firstNum} / ${secondNum} = ${firstNum / secondNum}`;
+
+if (secondNum == 0) {
+	division = "You cannot divide by 0";
+}
 
 alert(`
 ${firstNum} + ${secondNum} = ${firstNum + secondNum}
 ${firstNum} - ${secondNum} = ${firstNum - secondNum}
 ${firstNum} * ${secondNum} = ${firstNum * secondNum}
-${firstNum} / ${secondNum} = ${firstNum / secondNum}
+${division}
 `);
